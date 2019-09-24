77f499211e862af1e253e409775d831dfa0d446e
vkopendoh <vkopendoh@bk.ru>
2019-09-24 12:50:19 +0300

    Spring security default login form

---
+++ spring-security-demo-02-basic-security/src/main/java/com/vkopendoh/springsecurity/demo/config/SecurityWebAppInitializer.java
@@ -0,0 +1,7 @@
+package com.vkopendoh.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
